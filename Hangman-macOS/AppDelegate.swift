bc42c082eba7cfd6df8d2c0edc77bb99efbab6d1
Tobias Schaarschmidt <dev@tosc.dev>
2020-07-25 23:05:19 +0200

    Initial Commit

---
+++ Hangman-macOS/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  Hangman-macOS
+//  COPYRIGHT © 2020 Tobias Schaarschmidt
+//
+//  https://tosc.dev
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Hangman-macOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  Hangman-macOS
+//  COPYRIGHT © 2020 Tobias Schaarschmidt
+//
+//  https://tosc.dev
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
